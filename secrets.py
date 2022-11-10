911ffbec8cffcbcdae619199fe44132d2a30b632
Priyanshu Shah <105989704+MaDScienTisT21@users.noreply.github.com>
2022-11-09 22:47:02 -0500

    Add files via upload

---
+++ secrets.py
@@ -0,0 +1,9 @@
+#Keys
+
+#Spotify Devloper: https://developer.spotify.com/dashboard/
+#Redirtect Url: http://localhost:8888/callback Add this to your settings of your app
+#Playlist id: https://open.spotify.com/playlist/{YOUR-PLAYLIST-ID-HERE}
+
+client_id= "From Spotify Developer"
+client_secret="From Spotify Developer"
+playlist_id="Playlist ID"
